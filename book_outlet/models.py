3abf91566c50964091731349542ccfefcd309a0c
notenoughbees <excessiveamountofbees@gmail.com>
2024-12-10 14:18:49 +1300

    91: adding a slugfield & overwriting save()

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.urls import reverse
+from django.utils.text import slugify
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
@@ -8,10 +9,14 @@ class Book(models.Model):
         validators=[MinValueValidator(1), MaxValueValidator(5)])
     author = models.CharField(max_length=50, null=True)
     is_bestselling = models.BooleanField(default=False)
+    slug = models.SlugField(default="", null=False) # Harry Potter 1 -> harry-potter-1
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
 
     def get_absolute_url(self):
         return reverse("book-detail", args=[self.id])
-    
 
     def __str__(self):
         return f"{self.title} ({self.rating})"
