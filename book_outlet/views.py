000838b9a19a87be1135d9e15c34da540b9fc78a
notenoughbees <excessiveamountofbees@gmail.com>
2024-12-10 13:31:59 +1300

    89: rendering the details page

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,8 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Book
+from django.http import Http404
 
 def index(request):
     books = Book.objects.all()
     return render(request, "book_outlet/index.html", {
         "books": books
+    })
+
+
+def book_detail(request, id):
+    # try:
+    #     book = Book.objects.get(id=id)
+    # except:
+    #     raise Http404()
+
+    book = get_object_or_404(Book, id=id)
+
+    return render(request, "book_outlet/book_detail.html", {
+        "title": book.title,
+        "author": book.author,
+        "rating": book.rating,
+        "is_bestseller": book.is_bestselling
     })
