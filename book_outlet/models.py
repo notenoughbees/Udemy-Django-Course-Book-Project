08f6af4c62e072d99b6de5c1defe8fdf826b2157
notenoughbees <excessiveamountofbees@gmail.com>
2024-12-10 13:39:42 +1300

    90: model urls

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.urls import reverse
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
@@ -7,6 +8,10 @@ class Book(models.Model):
         validators=[MinValueValidator(1), MaxValueValidator(5)])
     author = models.CharField(max_length=50, null=True)
     is_bestselling = models.BooleanField(default=False)
+
+    def get_absolute_url(self):
+        return reverse("book-detail", args=[self.id])
+    
 
     def __str__(self):
         return f"{self.title} ({self.rating})"
